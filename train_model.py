b866fe3074653fd26e3f73f5e57ba52548e7ac63
Richmond <datamillers@gmail.com>
2026-02-18 17:51:20 +0000

    ML Pipeline exec Train model, save artifacts and ggenerate slice metrics

--- train_model.py
+++ train_model.py
@@ -13,14 +13,14 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "Your path here"
+project_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.read_csv(data_path) # your code here
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = None, None# Your code here
+train, test = train_test_split(data, test_size=0.20, random_state=42)# Your code here
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,7 +36,10 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    # your code here
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
     # use the train dataset 
     # use training=True
     # do not need to pass encoder and lb as input
@@ -52,7 +55,7 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+model = train_model(X_train, y_train) # your code here
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -66,7 +69,7 @@ model = load_model(
 ) 
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = None # your code here
+preds = inference(model, X_test) # your code here
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -80,6 +83,14 @@ for col in cat_features:
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
             # your code here
+            test,
+            col,
+            slicevalue,
+            cat_features,
+            label="salary",
+            encoder=encoder,
+            lb=lb,
+            model=model
             # use test, col and slicevalue as part of the input
         )
         with open("slice_output.txt", "a") as f:
