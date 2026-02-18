292a65774530b5b77bf98184c4ad95fb4fb32a58
Richmond <datamillers@gmail.com>
2026-02-18 16:17:28 +0000

    Implement performance on categorical slice

--- ml/model.py
+++ ml/model.py
@@ -128,11 +128,19 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    # slices the data
+    slice_df = data[data[column_name] == slice_value]
     X_slice, y_slice, _, _ = process_data(
         # your code here
+        slice_df,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
